3c20f93be67ec5477d7128eec9b40bacbe7f76b4
Chris Goller <goller@gmail.com>
2020-04-17 11:27:36 -0500

    refactor: use single script file for simplicity
    Signed-off-by: Chris Goller <goller@gmail.com>

--- script.sh
+++ script.sh
@@ -7,9 +7,21 @@ export NAME=$(buildkite-agent meta-data get name)
 export REGION=$(buildkite-agent meta-data get region)
 buildkite-agent meta-data get cidrs
 
-echo "+++ :package: terraform"
-
+echo "+++ :package: terraform init"
 terraform init
+
+echo "+++ :package: terraform plan"
 terraform plan -var="region=${REGION}" -out=${NAME}.plan
-terraform apply ${NAME}.plan
+
+echo "+++ :package: terraform apply"
+terraform apply -auto-approve ${NAME}.plan
+
 cp terraform.tfstate /tmp
+
+echo "+++ :package: endpoint artifact"
+cat terraform.tfstate |jq  '{endpoint: .outputs.cluster_endpoint.value}'
+buildkite-agent artifact upload endpoint.json
+
+echo "+++ :package: kubeconfig artifact"
+cat terraform.tfstate |jq -r .outputs.kube_config.value > kubeconfig.yaml
+buildkite-agent artifact upload kubeconfig.yaml
