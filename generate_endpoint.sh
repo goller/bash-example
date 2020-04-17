2220b025dc239f6e3500dda31ea5d078743c88b2
Chris Goller <goller@gmail.com>
2020-04-17 11:28:10 -0500

    refactor: remove generate scripts
    Signed-off-by: Chris Goller <goller@gmail.com>

--- generate_endpoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-cat terraform.tfstate |jq  '{endpoint: .outputs.cluster_endpoint.value}'
-buildkite-agent artifact upload endpoint.json
--- generate_kubeconfig.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-cat terraform.tfstate |jq -r .outputs.kube_config.value > kubeconfig.yaml
-buildkite-agent artifact upload kubeconfig.yaml
